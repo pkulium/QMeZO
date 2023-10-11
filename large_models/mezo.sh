daf0b7e66a005e12beda2d8ede1eae426da6e539
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-11 12:11:33 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/mezo.sh
    	modified:   large_models/test.sh

--- large_models/mezo.sh
+++ large_models/mezo.sh
@@ -51,35 +51,16 @@ echo "TRAIN/EVAL STEPS: $STEPS/$EVAL_STEPS"
 echo "MODE: $MODE"
 echo "Extra args: $EXTRA_ARGS $TASK_ARGS"
 
-
-if [ "$DEBUG" == "True" ]; then
-    python -m pdb run.py \
-        --model_name $MODEL \
-        --task_name $TASK \
-        --output_dir result/$TASK-${MODEL_NAME}-$TAG --tag $TAG --train_set_seed $SEED --num_train $TRAIN --num_dev $DEV --num_eval $EVAL --logging_steps 10 \
-        --max_steps $STEPS \
-        --trainer zo --load_float16 \
-        --learning_rate $LR --zo_eps $EPS --per_device_train_batch_size $BS --lr_scheduler_type "constant" \
-        --load_best_model_at_end --evaluation_strategy steps --save_strategy steps --save_total_limit 1 \
-        --eval_steps $EVAL_STEPS --save_steps $EVAL_STEPS \
-        --train_as_classification \
-        $EXTRA_ARGS \
-        $TASK_ARGS \
-        "$@"
-elif [ "$DEBUG" == "False" ]; then
-    python run.py \
-        --model_name $MODEL \
-        --task_name $TASK \
-        --output_dir result/$TASK-${MODEL_NAME}-$TAG --tag $TAG --train_set_seed $SEED --num_train $TRAIN --num_dev $DEV --num_eval $EVAL --logging_steps 10 \
-        --max_steps $STEPS \
-        --trainer zo --load_float16 \
-        --learning_rate $LR --zo_eps $EPS --per_device_train_batch_size $BS --lr_scheduler_type "constant" \
-        --load_best_model_at_end --evaluation_strategy steps --save_strategy steps --save_total_limit 1 \
-        --eval_steps $EVAL_STEPS --save_steps $EVAL_STEPS \
-        --train_as_classification \
-        $EXTRA_ARGS \
-        $TASK_ARGS \
-        "$@"
-
-    
- 
+python -m pdb run.py \
+    --model_name $MODEL \
+    --task_name $TASK \
+    --output_dir result/$TASK-${MODEL_NAME}-$TAG --tag $TAG --train_set_seed $SEED --num_train $TRAIN --num_dev $DEV --num_eval $EVAL --logging_steps 10 \
+    --max_steps $STEPS \
+    --trainer zo --load_float16 \
+    --learning_rate $LR --zo_eps $EPS --per_device_train_batch_size $BS --lr_scheduler_type "constant" \
+    --load_best_model_at_end --evaluation_strategy steps --save_strategy steps --save_total_limit 1 \
+    --eval_steps $EVAL_STEPS --save_steps $EVAL_STEPS \
+    --train_as_classification \
+    $EXTRA_ARGS \
+    $TASK_ARGS \
+    "$@"
