bde26b816266a6571075c09cdbf6c15e590e10a3
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-11 11:41:49 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   gptq/opt.py
    	modified:   gptq/test.sh

--- gptq/test.sh
+++ gptq/test.sh
@@ -1,4 +1,3 @@
 git pull
 # CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-13b c4 --wbits 2 --groupsize 1024 --save opt13-2bit.pt
-# CUDA_VISIBLE_DEVICES=0 python -m pdb opt.py opt13-2bit.pt c4  
-CUDA_VISIBLE_DEVICES=0 python -m pdb opt.py --load opt13-2bit.pt   
+CUDA_VISIBLE_DEVICES=0 python opt.py facebook/opt-13b c4 --load opt13-2bit.pt   
