e8a521fcd28128b9b5bcdeb4f929ce5f34013788
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-15 15:14:50 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/run.py

--- large_models/run.py
+++ large_models/run.py
@@ -216,34 +216,6 @@ def add_mezo_parts(model):
             module.mezo_part = mezo_part
             module.forward = custom_forward.__get__(module)
 
-
-def get_wikitext2(nsamples, seed, seqlen, model):
-    from datasets import load_dataset
-    traindata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
-    testdata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
-
-    from transformers import AutoTokenizer
-    try:
-        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
-    except:
-        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=True)
-    trainenc = tokenizer("\n\n".join(traindata['text']), return_tensors='pt')
-    testenc = tokenizer("\n\n".join(testdata['text']), return_tensors='pt')
-
-    import random
-    random.seed(seed)
-    np.random.seed(0)
-    torch.random.manual_seed(0)
-    
-    traindataset = []
-    for _ in range(nsamples):
-        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
-        j = i + seqlen
-        inp = trainenc.input_ids[:, i:j]
-        attention_mask = torch.ones_like(inp)
-        traindataset.append({'input_ids':inp,'attention_mask': attention_mask})
-    return traindataset, testenc
-
 class Framework:
 
     def __init__(self, args, task):
@@ -291,33 +263,9 @@ class Framework:
             #         max_memory={i: f'{free_in_GB-5}GB' for i in range(torch.cuda.device_count())},
             #         load_in_8bit=self.args.load_int8,
             #     )
-
-            # quantized_model_dir = '/work/LAS/wzhang-lab/mingl/code/QMeZO/AutoGPTQ/examples/quantization/opt-13b-2bit-128g'
-            # from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-            # quantize_config = BaseQuantizeConfig(
-            #     bits=2,  # quantize model to 4-bit
-            #     group_size=128,  # it is recommended to set the value to 128
-            #     desc_act=False,  # desc_act and group size only works on triton
-            # )
-            # model = AutoGPTQForCausalLM.from_quantized(quantized_model_dir, device="cuda:0", use_triton=False)
-
-            from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-            pretrained_model_dir = "facebook/opt-13b"
-            tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True)
-            traindataset,testenc = get_wikitext2(128, 0, 2048, pretrained_model_dir)
-
-            quantize_config = BaseQuantizeConfig(
-                bits=2,  # quantize model to 4-bit
-                group_size=128,  # it is recommended to set the value to 128
-                desc_act=False,  # desc_act and group size only works on triton
-            )
-
-            # load un-quantized model, the model will always be force loaded into cpu
-            model = AutoGPTQForCausalLM.from_pretrained(pretrained_model_dir, quantize_config)
-
-            # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask" 
-            # with value under torch.LongTensor type.
-            model.quantize(traindataset, use_triton=False)
+            quantized_model_dir = '/work/LAS/wzhang-lab/mingl/code/QMeZO/AutoGPTQ/examples/quantization/opt-13b-2bit-128g'
+            from auto_gptq import AutoGPTQForCausalLM
+            model = AutoGPTQForCausalLM.from_quantized(quantized_model_dir, device="cuda:0", use_triton=False)
             model.eval()
             add_mezo_parts(model)
 
