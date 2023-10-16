d5e02574782f55404e73cd555b42fba90c6babbf
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-16 10:01:46 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/mezo.sh

--- large_models/mezo.sh
+++ large_models/mezo.sh
@@ -59,7 +59,7 @@ echo "TRAIN/EVAL STEPS: $STEPS/$EVAL_STEPS"
 echo "MODE: $MODE"
 echo "Extra args: $EXTRA_ARGS $TASK_ARGS"
 
-python -m pdb run.py \
+python run.py \
     --model_name $MODEL \
     --task_name $TASK \
     --output_dir result/$TASK-${MODEL_NAME}-$TAG --tag $TAG --train_set_seed $SEED --num_train $TRAIN --num_dev $DEV --num_eval $EVAL --logging_steps 10 \
