9694e625ef2219ee462e592a62fb9f81c27af86c
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-28 11:15:16 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   AutoGPTQ/examples/quantization/example_our1.py

--- AutoGPTQ/examples/quantization/example_our1.py
+++ AutoGPTQ/examples/quantization/example_our1.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
 import torch
 
-model_id = "facebook/opt-13b"
+model_id = "facebook/opt-125m"
 
 quantization_config = GPTQConfig(
      bits=2,
@@ -12,4 +12,4 @@ quantization_config = GPTQConfig(
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 quant_model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config, device_map='auto')
-quant_model.save_pretrained("opt-13b-gptq")
+quant_model.save_pretrained("opt-125m-gptq")
