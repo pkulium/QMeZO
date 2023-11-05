a62c20414fe7826e19b08303bd509c653fcb6e0e
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-05 13:49:13 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/run.py

--- large_models/run.py
+++ large_models/run.py
@@ -227,7 +227,7 @@ def custom_forward(self, x):
             weight = (scales * (weight - zeros))
             weight = weight.reshape(weight.shape[0] * weight.shape[1], weight.shape[2])
 
-            quantized_mezo_weight = quantize(self.mezo_part.weight, bits=8)        
+            quantized_mezo_weight = quantize(self.mezo_part.weight, bits=4)        
             weight += quantized_mezo_weight.reshape(weight.shape)
             out = torch.matmul(x.half(), weight)
         out = out.half().reshape(out_shape)
