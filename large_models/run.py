ef8f9882ea61cd35bd7881e82f7d8fa18ffb2d16
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-05 16:19:29 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/run.py

--- large_models/run.py
+++ large_models/run.py
@@ -593,6 +593,8 @@ class Framework:
             train_dataset=train_dataset, 
             eval_dataset=eval_dataset,
             tokenizer=self.tokenizer,
+            # add label_names to get loss
+            label_names = ['labels'],
             data_collator=DataCollatorWithPaddingAndNesting(self.tokenizer, pad_to_multiple_of=8) if self.args.train_as_classification else collator(self.tokenizer, pad_to_multiple_of=8),
         )
         if self.args.save_on_interrupt:
