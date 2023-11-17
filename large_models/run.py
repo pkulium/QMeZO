81942c912f489a17e1880154e710679ca99150ce
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-17 16:13:31 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/run.py

--- large_models/run.py
+++ large_models/run.py
@@ -461,11 +461,15 @@ def reset_parameters(self):
     if hasattr(self, 'lora_A'):
         # initialize A the same way as the default for nn.Linear and B to zero
         if DEBUG:
+            lora_A = nn.Parameter(self.bias.new_zeros((5, 5)))
+            logging.info(f'Before:{lora_A}')
             logging.info(f'Before:{self.lora_A}')
         nn.init.normal_(self.lora_A)
         nn.init.zeros_(self.lora_B)
         if DEBUG:
+            nn.init.normal_(lora_A)
             logging.info(f'After:{self.lora_A}')
+            logging.info(f'After:{lora_A}')
 
 def add_mezo_lora_parts(model, r, alpha, float16):
     if model.config.model_type == "opt":
