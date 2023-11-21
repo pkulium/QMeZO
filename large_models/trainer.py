0c7f4d93ca177a695502a0bd1c290ce0233fe31e
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-21 14:50:57 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/trainer.py

--- large_models/trainer.py
+++ large_models/trainer.py
@@ -833,11 +833,9 @@ class OurTrainer(Trainer):
                     # quantizer = self.name_to_mezo_part[name].quantizer
                     # qweight, absmax, _ = quantizer.quantize_block(param.data)
                     # param.data = quantizer.dequantize_block(qweight, absmax, quantizer.weight_size)
-                    n_bits = 2
                     # quantized_tensor, min_val, max_val = custom_quantize(param.data, n_bits)
                     # param.data = custom_dequantize(quantized_tensor, min_val, max_val, n_bits)
-                    param.data = quantize_nbit(param.data, n_bits)
-                    
+                    param.data = quantize_nbit(param.data, n_bits = 2)
         self.lr_scheduler.step()
 
 
