88eeda6aaeeac41e45ba3ec6977c3c847ddfa879
pkulium <54947136+pkulium@users.noreply.github.com>
2023-12-01 14:58:59 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/grid_search.sh
    	modified:   large_models/test.sh

--- large_models/grid_search.sh
+++ large_models/grid_search.sh
@@ -2,7 +2,7 @@
 
 # Define arrays of values to try
 learning_rates=(1e-6 1e-7 4e-7 8e-7)
-epsilons=(1e-3 5e-3 1e-4 5e-4)
+epsilons=(1e-3 2e-3 1e-4 5e-4)
 
 # Loop over all combinations
 for lr in "${learning_rates[@]}"; do
