00718d1d1c7927df13e89478468cda31d608efa7
pkulium <54947136+pkulium@users.noreply.github.com>
2023-12-14 08:44:51 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   large_models/mezos.sh

---
+++ large_models/mezos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Define the list of tasks
+TASKS=("SST2" "RTE" "CB" "BoolQ" "WSC" "WIC" "MultiRC" "Copa" "ReCoRD" "SQuAD" "DROP")
+
+# Loop over each task
+for TASK in "${TASKS[@]}"; do
+   echo "Running script for TASK: $TASK"
+
+   # Call your existing script here, passing TASK as an argument
+   # Assuming your script is named 'run_training.sh'
+   TASK=$TASK sh ./mezo.sh
+done
