adbe5cff943a53847442115042890fa7feb08c33
pkulium <54947136+pkulium@users.noreply.github.com>
2023-12-28 18:38:11 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   large_models/lora.sh

--- large_models/lora.sh
+++ large_models/lora.sh
@@ -2,8 +2,8 @@ git pull
 MODEL=facebook/opt-13b
 # TASK=SST2
 MODE=lora 
-LR=7e-5
-EPS=1e-2 
+LR=5e-5
+EPS=1e-3
 MODEL=${MODEL:-facebook/opt-13b}
 MODEL_NAME=(${MODEL//\// })
 MODEL_NAME="${MODEL_NAME[-1]}"
